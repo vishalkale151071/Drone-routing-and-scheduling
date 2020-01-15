94298c8b50a5ce2852d02bfeb12ea5b718bb7fed
vishal kale <vishalkale151071@gmail.com>
2020-01-15 19:20:46 +0530

    Final Push.

--- get_points.py
+++ get_points.py
@@ -4,21 +4,42 @@ import shapefile
 import numpy as np
 import node
 
-drones = 3
+drones = 5
 
 polygon = None
-sf = shapefile.Reader("shapefiles/midc")
+poly_points = None
+
+
+def line_intersection(l1, l2):
+    dx = (l1[0][0] - l1[1][0], l2[0][0] - l2[1][0])
+    dy = (l1[0][1] - l1[1][1], l2[0][1] - l2[1][1])
+
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(dx, dy)
+    if div == 0:
+       print('lines do not intersect')
+
+    d = (det(*l1), det(*l2))
+    x = det(d, dx) / div
+    y = det(d, dy) / div
+    return x, y
+
+
+sf = shapefile.Reader("shapefiles/dyp")
 for shape in list(sf.iterShapes()):
     npoints=len(shape.points) # total points
     nparts = len(shape.parts) # total parts
     polygon = Polygon(shape.points)
+    poly_points = shape.points
     if nparts == 1:
         x_lon = np.zeros((len(shape.points),1))
         y_lat = np.zeros((len(shape.points),1))
         for ip in range(len(shape.points)):
             x_lon[ip] = shape.points[ip][0]
             y_lat[ip] = shape.points[ip][1]
-        plt.plot(x_lon, y_lat)
+        #plt.plot(x_lon, y_lat)
 
 x_min, y_min, x_max, y_max = polygon.bounds
 
@@ -68,10 +89,16 @@ for e,x in enumerate(bitmap):
         temp = i.get_points()
         x_array.append(temp[0])
         y_array.append(temp[1])
-
+index = len(x_array)
+for i in range(0,len(poly_points)-1):
+    result = filter(lambda x: (x <= poly_points[i][1] and x >= poly_points[i+1][1]) or (x >= poly_points[i][1] and x <= poly_points[i+1][1]), y_range)
+    for line in result:
+        pt = line_intersection((poly_points[i],poly_points[i+1]),((0,line),(x_max,line)))
+        x_array.insert(index, pt[0])
+        y_array.insert(index, pt[1])
 lenght = len(x_array)
 part = lenght//drones
-start , end = 0, part
+start, end = 0, part
 
 clr = ['green', 'orange', 'cyan', 'red', 'coral', 'green', 'orange', 'cyan', 'red', 'coral']
 for i in range(0, drones):
@@ -81,8 +108,8 @@ for i in range(0, drones):
     else:
         x = x_array[start:end]
         y = y_array[start:end]
+
     plt.plot(x, y, color=clr[i])
-    plt.pause(1)
     start = end - 1
     end = end + part
-# plt.show()
+plt.show()

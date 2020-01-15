7d168db6d88fe7c2d8d7e11d45f7d5b1428fe1bd
vishal kale <vishalkale151071@gmail.com>
2020-01-15 11:57:21 +0530

    line drawn

--- get_points.py
+++ get_points.py
@@ -16,6 +16,7 @@ for shape in list(sf.iterShapes()):
         for ip in range(len(shape.points)):
             x_lon[ip] = shape.points[ip][0]
             y_lat[ip] = shape.points[ip][1]
+        plt.plot(x_lon, y_lat)
 
 x_min = x_lon.min()
 x_max = x_lon.max()
@@ -26,19 +27,39 @@ bitmap = []
 row = []
 x_range = np.arange(x_min, x_max, 0.0001)
 y_range = np.arange(y_min, y_max, 0.0001)
-for i in range(0, len(x_range)):
-    row.clear()
-    for j in range(0, len(y_range)):
+for j in range(0, len(y_range)):
+    row = []
+    for i in range(0, len(x_range)):
         point = None
         if polygon.contains(Point(x_range[i], y_range[j])):
-            plt.scatter(x_range[i], y_range[j], s =.5, c='b')
+            plt.scatter(x_range[i], y_range[j], s =.5, c='blue')
             point = node.Node(x_range[i], y_range[j], True)
         else:
-            plt.scatter(x_range[i], y_range[j], s=.5, c='r')
+            plt.scatter(x_range[i], y_range[j], s=.5, c='red')
             point = node.Node(x_range[i], y_range[j], False)
         row.append(point)
     bitmap.append(row)
-plt.show()
 
-data = np.array(bitmap)
-print(data.shape )
+for j in range(0, len(y_range)):
+    for i in range(0, len(x_range)):
+
+
+shape = np.array(bitmap).shape
+x_array = []
+y_array = []
+for e,x in enumerate(bitmap):
+    row = []
+    for y in x:
+        if y.get_state():
+            row.append(y)
+    if e % 2 != 0:
+        row.reverse()
+
+
+    for i in row:
+        temp = i.get_points()
+        x_array.append(temp[0])
+        y_array.append(temp[1])
+
+plt.plot(x_array, y_array, c='green')
+plt.show()
